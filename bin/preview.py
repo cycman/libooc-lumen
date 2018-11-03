3022c806614d8296bba82c476c0e5f83c0079026
yunchun.chen@baishancloud.com <yunchun.chen@baishancloud.com>
2018-11-04 06:06:55 +0800

    change: python

--- bin/preview.py
+++ bin/preview.py
@@ -1,22 +1,22 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader, utils
 import sys
-from cStringIO import StringIO
+from io import BytesIO
 import subprocess
 
 def decompress_pdf(temp_buffer):
     temp_buffer.seek(0)  # Make sure we're at the start of the file.
 
-    process = subprocess.Popen(['pdftk.exe',
+    process = subprocess.Popen(['C:\\Users\\Administrator\\Desktop\\libooc-lumen\\bin\\PDFtk\\bin\\pdftk.exe',
                                 '-',  # Read from stdin.
                                 'output',
                                 '-',  # Write to stdout.
                                 'uncompress'],
                                 stdin=temp_buffer,
                                 stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
+                                stderr=subprocess.PIPE,shell=True)
     stdout, stderr = process.communicate()
 
-    return StringIO(stdout)
+    return BytesIO(stdout)
 # 开始页
 start_page = int(sys.argv[1])
 
@@ -27,12 +27,12 @@ end_page = int(sys.argv[2])
 
 output = PdfFileWriter()
 with open(sys.argv[3], 'rb') as input_file:
-    input_buffer = StringIO(input_file.read())
+    input_buffer = BytesIO(input_file.read())
 
 try:
     pdf_file = PdfFileReader(input_buffer)
 except utils.PdfReadError:
-    pdf_file = PdfFileReader(decompress_pdf(input_file))
+    pdf_file = PdfFileReader(decompress_pdf(open(sys.argv[3], 'rb')))
 pdf_pages_len = pdf_file.getNumPages()
 
 for i in range(start_page, end_page):
