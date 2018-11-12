e712cb43be207052ceecbc75979f4913832ba72a
yunchun.chen@baishancloud.com <yunchun.chen@baishancloud.com>
2018-11-12 19:25:13 +0800

    niu

--- app/Resource/NiuTranslateResource.php
+++ app/Resource/NiuTranslateResource.php
@@ -12,30 +12,23 @@ namespace App\Resource;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
 
-class QinghuaTranslateResource
+class NiuTranslateResource
 {
-    private $url = "http://101.6.5.207:3893/translateapi";
+    private $url = "http://127.0.0.1:1517/niutrans/translation";
 
     public function translateEnToZh($queries = [])
     {
         $query = implode(" lvbs ", $queries);
-        $url = sprintf("%s/%s", $this->url, rawurlencode(rawurlencode($query)));
+        $url = sprintf("%s?from=zh&to=zh&src_text=%s", $this->url, (urlencode($query)));
         $ch = curl_init();
-        $headers = [
-            'Host: 101.6.5.207:3893',
-            'Origin: http://101.6.5.207:3892',
-            'Referer: http://101.6.5.207:3892/',
-            'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
-        ];
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_TIMEOUT, 10);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
         $r = curl_exec($ch);
         curl_close($ch);
-        $r = str_replace("\u3002", '', $r);
+        $r = str_replace("\n", '', $r);
         $result = json_decode($r, true);
         return explode("lvbs", $result['trg']);
     }
